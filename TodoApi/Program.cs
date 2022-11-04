8d4a2e2c7f592347679c23abd3027d786724226b
Thamires Silva <thamires.silva@autotrac.com.br>
2022-11-04 08:33:21 -0300

    teste com scanner local

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -137,7 +137,6 @@ class TarefaDb : DbContext
     //     modelBuilder.Entity<Tarefa>().HasData(new Tarefa { Nome = "Fazer Code Review", FoiConcluida = false });
     //     modelBuilder.Entity<Tarefa>().HasData(new Tarefa { Nome = "Modelar Banco de Dados", FoiConcluida = true });
     // }
-
 }
 static class teste
 {
