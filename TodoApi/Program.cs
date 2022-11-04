fa07d111a33d5fd7d69414252792e951948b1c3b
Thamires Silva <thamires.silva@autotrac.com.br>
2022-11-03 22:40:47 -0300

    atualização com exemplos

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,7 +1,8 @@
+
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDbContext<TarefaDb>(opt => opt.UseInMemoryDatabase("TodoList"));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddEndpointsApiExplorer();
@@ -13,103 +14,143 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
 }
 
 app.UseHttpsRedirection();
-app.MapGet("/", () => "Sextooooou!");
+app.MapGet("/", (TarefaDb db) =>
+{
+      var lista = new List<Tarefa>{
+new Tarefa { Nome = "Analisar Logs", FoiConcluida = true },
+new Tarefa { Nome = "Cadastrar Cartão de Suporte no Kanban", FoiConcluida = false },
+new Tarefa { Nome = "Fazer Code Review", FoiConcluida = false },
+new Tarefa { Nome = "Modelar Banco de Dados", FoiConcluida = true }};
+        db.Tarefas.AddRange(lista);
 
-app.MapGet("/data-only", () => new DateTime());
+        db.SaveChanges();
+    return Results.Ok();
+});
 
-app.MapGet("/time-only", () => new TimeOnly());
+app.MapGet("/data", () => new DateTime());
 
-app.MapGet("/todoitems", async (TodoDb db) =>
-    await db.Todos.ToListAsync());
+app.MapGet("/tarefas", async (TarefaDb db) =>
+    await db.Tarefas.ToListAsync());
 
-app.MapGet("/todoitems/complete", async (TodoDb db) =>
-    await db.Todos.Where(t => t.IsComplete).ToListAsync());
+app.MapGet("/tarefas/concluidas", async (TarefaDb db) =>
+    await db.Tarefas.Where(t => t.FoiConcluida).ToListAsync());
 
-app.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
-    await db.Todos.FindAsync(id)
-        is Todo todo
+app.MapGet("/tarefas/nao-concluidas", async (TarefaDb db) =>
+    await db.Tarefas.Where(t => !t.FoiConcluida).ToListAsync());
+
+app.MapGet("/tarefa/{id}", async (int id, TarefaDb db) =>
+    await db.Tarefas.FindAsync(id)
+        is Tarefa todo
             ? Results.Ok(todo)
            // : Results.NotFound());
            : Results.NoContent());//204
-           
-             app.MapGet("/todoitems/by-name/{name}", async (string name, TodoDb db) =>
-    await db.Todos.Where(t=> t.Name.Equals(name)).ToListAsync()
-        is List<Todo> todos
-            ? Results.Ok(todos)
-           // : Results.NotFound());
-           : Results.NoContent());//204
 
-app.MapPost("/todoitems", async (Todo todo, TodoDb db) =>
+app.MapGet("/tarefa/por-nome/{nome}", async (string nome, TarefaDb db) =>
+await db.Tarefas.Where(t => t.Nome == nome).ToListAsync()
+is List<Tarefa> todos
+? Results.Ok(todos)
+// : Results.NotFound());
+: Results.NoContent());//204
+
+app.MapPost("/tarefas", async (Tarefa tarefa, TarefaDb db) =>
 {
-    if (!todo.IsValid())
-       { return Results.UnprocessableEntity();}
+    if (!tarefa.IsValid())
+        return Results.UnprocessableEntity();//422
 
-    db.Todos.Add(todo);
+    db.Tarefas.Add(tarefa);
     await db.SaveChangesAsync();
 
-    return Results.Created($"/todoitems/{todo.Id}", todo);
+    return Results.Created($"/tarefas/{tarefa.Id}", tarefa);
 });
 
 
-app.MapPut("/todoitems/{id}", async (int id, Todo inputTodo, TodoDb db) =>
+app.MapPut("/tarefas/{id}", async (int id, Tarefa tarefaAlterada, TarefaDb db) =>
 {
-    if (!inputTodo.IsValid())
+    if (!tarefaAlterada.IsValid())
         return Results.UnprocessableEntity();
 
-    var todo = await db.Todos.FindAsync(id);
+    var tarefa = await db.Tarefas.FindAsync(id);
 
-    if (todo is null) return Results.NotFound();
-    //if (todo is null) return Results.NoContent();//204
+    //if (todo is null) return Results.NotFound();
+    if (tarefa is null) return Results.NoContent();//204
 
-    todo.Name = inputTodo.Name;
-    todo.IsComplete = inputTodo.IsComplete;
+    tarefa.Nome = tarefaAlterada.Nome;
+    tarefa.FoiConcluida = tarefaAlterada.FoiConcluida;
 
     await db.SaveChangesAsync();
 
     return Results.NoContent();
 });
-app.MapGet("/todoitems/numero/{id}", async (int id) =>
-{
-   id =+ 1;
-   return Results.Ok(id);
-});
 
-app.MapDelete("/todoitems/{id}", async (int id, TodoDb db) =>
+app.MapDelete("/tarefas/{id}", async (int id, TarefaDb db) =>
 {
-    if (await db.Todos.FindAsync(id) is Todo todo)
+    if (await db.Tarefas.FindAsync(id) is Tarefa tarefa)
     {
-        db.Todos.Remove(todo);
+        db.Tarefas.Remove(tarefa);
         await db.SaveChangesAsync();
-        return Results.Ok(todo);
+        return Results.Ok(tarefa);
     }
     //return Results.BadRequest();//400
     return Results.NotFound();//404
-   // return Results.NoContent();//204
+                              // return Results.NoContent();//204
 });
+
 
 app.Run();
 
-class Todo
+class Tarefa
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public bool IsComplete { get; set; }
+    public string Nome { get; set; }
+    public bool FoiConcluida { get; set; }
     public bool IsValid()
     {
-        if (!string.IsNullOrEmpty(Name))
+        if (!string.IsNullOrEmpty(Nome))
             return true;
 
         return false;
     }
 }
 
-class TodoDb : DbContext
-{
-    public TodoDb(DbContextOptions<TodoDb> options)
-        : base(options) { }
 
-    public DbSet<Todo> Todos => Set<Todo>();
+class TarefaDb : DbContext
+{
+    public TarefaDb(DbContextOptions<TarefaDb> options)
+        : base(options)
+    {
+
+        // this.Add(new Todo{Nome = "Analisar Logs", IsComplete = true});
+        // this.SaveChanges();
+
+    }
+
+    public DbSet<Tarefa> Tarefas => Set<Tarefa>();
+    // protected override void OnModelCreating(ModelBuilder modelBuilder)
+    // {
+
+    //     modelBuilder.Entity<Tarefa>().HasData(new Tarefa { Nome = "Analisar Logs", FoiConcluida = true });
+    //     modelBuilder.Entity<Tarefa>().HasData(new Tarefa { Nome = "Cadastrar Cartão de Suporte no Kanban", FoiConcluida = false });
+    //     modelBuilder.Entity<Tarefa>().HasData(new Tarefa { Nome = "Fazer Code Review", FoiConcluida = false });
+    //     modelBuilder.Entity<Tarefa>().HasData(new Tarefa { Nome = "Modelar Banco de Dados", FoiConcluida = true });
+    // }
+}
+static class teste
+{
+    public static void AddTestData(TarefaDb context)
+    {
+
+
+        var lista = new List<Tarefa>{
+new Tarefa { Nome = "Analisar Logs", FoiConcluida = true },
+new Tarefa { Nome = "Cadastrar Cartão de Suporte no Kanban", FoiConcluida = false },
+new Tarefa { Nome = "Fazer Code Review", FoiConcluida = false },
+new Tarefa { Nome = "Modelar Banco de Dados", FoiConcluida = true }};
+        context.Tarefas.AddRange(lista);
+
+        context.SaveChanges();
+    }
 }
